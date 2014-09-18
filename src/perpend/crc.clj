7a09e8e55e3443197ca6557adecec0917dc8f7ff
Matt Gauger <matt.gauger@gmail.com>
2014-09-18 13:50:13 -0500

    Non-working table-generation ported from C code.
    - There's no reason to create the lookup table right now; I'm not
      worrying about optimizing.
    - I will instead try to inline what the lookup would get for each step.
    - Use Rosetta-code reference implementation in C

--- src/perpend/crc.clj
+++ src/perpend/crc.clj
@@ -8,10 +8,23 @@
 ;; Using java.util.zip's CRC32 implementation:
 (defn ref-crc32 [s]
   (let [crc (new java.util.zip.CRC32)
-        bytes-array (.getBytes s)]
+	bytes-array (.getBytes s)]
     (do
       (.update crc bytes-array 0 (count bytes-array))
       (.getValue crc))))
 
+(defn crc32-table-inner [i]
+  (loop [j 8
+         rem i]
+    (if (zero? j)
+      rem
+      (if (bit-and rem 1)
+        (recur (dec j) (bit-xor 0xEDB88320 (bit-shift-right 1 rem)))
+        (recur (dec j) (bit-shift-right 1 rem))))))
+
+(def crc32-lookup-table
+  (map crc32-table-inner (range 256)))
+
 (defn crc32 [s]
+  (let [polynomial 0x04C11DB7])
   s)
--- test/perpend/crc_test.clj
+++ test/perpend/crc_test.clj
@@ -10,5 +10,10 @@
 (deftest ref-crc32-tests
   (fact (ref-crc32 "abcde") => 2240272485))
 
+;; values come from the C implementation in the src/ dir
+(deftest crc32-tests
+  (let [quick "The quick brown fox jumps over the lazy dog"
+        expected-hex 0x414FA339]
+    (fact (crc32 quick) => expected-hex)))
 
 
